9362d1af77c434d0049eb83486e5fe5d4e30623c
Will Stone <654103+will-stone@users.noreply.github.com>
2018-10-20 12:35:07 +0100

    docs: add dev tools to demo

--- demo/index.js
+++ demo/index.js
@@ -1,13 +1,18 @@
 import { Provider } from 'mobx-react'
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import App from './components/App'
 import store from './store'
+import { configureDevtool } from 'mobx-react-devtools'
+
+configureDevtool({
+  logEnabled: true,
+  updatesEnabled: false,
+})
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
