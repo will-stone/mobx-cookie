ab91ff553718208d0ec0c5ca2ecb5e61f617c130
Will Stone <654103+will-stone@users.noreply.github.com>
2018-10-20 12:38:23 +0100

    demo: use semantic names for actions. remove moment

--- demo/components/App.js
+++ demo/components/App.js
@@ -1,19 +1,17 @@
 import { inject, observer } from 'mobx-react'
 import React from 'react'
 
-import moment from 'moment'
-
 const App = ({ store }) => {
   return (
     <div>
-      <pre>Cookie: {store.cookie.get()}</pre>
+      <pre>Cookie: {store.timestamp}</pre>
       <div>
-        <button onClick={() => store.setCookie(moment().format('x'))}>
+        <button onClick={() => store.setTimestamp(+new Date())}>
           Set cookie to current timestamp
         </button>
       </div>
       <div>
-        <button onClick={() => store.removeCookie()}>Remove cookie</button>
+        <button onClick={store.unsetTimestamp}>Remove cookie</button>
       </div>
     </div>
   )
--- demo/store.js
+++ demo/store.js
@@ -1,24 +1,29 @@
-import { action, decorate, observable } from 'mobx'
+import { action, decorate, observable, computed } from 'mobx'
 import Cookie from '../src/mobx-cookie'
 
 class Store {
   cookie = new Cookie('cookie')
 
-  setCookie = value => {
+  get timestamp() {
+    return this.cookie.value
+  }
+
+  setTimestamp = value => {
     const currentTime = new Date()
     const Time = currentTime.setTime(currentTime.getTime() + 1000 * 10)
     this.cookie.set(value, { expires: new Date(Time) })
   }
 
-  removeCookie = () => {
+  unsetTimestamp = () => {
     this.cookie.remove()
   }
 }
 
 decorate(Store, {
   cookie: observable,
-  setCookie: action,
-  removeCookie: action,
+  timestamp: computed,
+  setTimestamp: action,
+  unsetTimestamp: action,
 })
 
 export default new Store()
