7df987a014e1d6ed763fec05489616a0aaa23ba8
Will Stone <654103+will-stone@users.noreply.github.com>
2018-10-20 11:28:01 +0100

    refactor: demo store updated to use mobx4 decorate

--- demo/store.js
+++ demo/store.js
@@ -1,23 +1,18 @@
-import { action, extendObservable, decorate, observable } from 'mobx'
-
+import { action, decorate, observable } from 'mobx'
 import Cookie from '../src/mobx-cookie'
 
 class Store {
-  constructor() {
-    extendObservable(this, {
-      cookie: new Cookie('cookie'),
-    })
-  }
+  cookie = new Cookie('cookie')
 
-  setCookie = action('Set cookie', value => {
+  setCookie = value => {
     const currentTime = new Date()
     const Time = currentTime.setTime(currentTime.getTime() + 1000 * 10)
     this.cookie.set(value, { expires: new Date(Time) })
-  })
+  }
 
-  removeCookie = action('Remove cookie', value => {
+  removeCookie = () => {
     this.cookie.remove()
-  })
+  }
 }
 
 decorate(Store, {
